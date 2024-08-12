ba1198710a25366844dee3f3800120f26fc58a01
Israel Goldstein <israelgol269@gmail.com>
2024-08-12 13:26:57 -0400

    Update import statement for employee controller in employee route

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userLogin, userClockin, userClockout, getEmployeeRecords } from '../controllers/employee';
+import { userLogin, userClockin, userClockout, getEmployeeRecords } from '../controllers/employee.js';
 const router = express.Router();
 export default router;
 
