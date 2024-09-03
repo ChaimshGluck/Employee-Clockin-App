9cdf62f268dc42454e886fecad6b85f1b54c0007
Chaim Gluck <chaimshglick@gmail.com>
2024-09-03 01:32:07 -0400

    static folder

--- server/index.js
+++ server/index.js
@@ -1,24 +1,28 @@
 import express from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser'
-import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import hrRoutes from './routes/hr.js';
 import employeeRoutes from './routes/employee.js';
 
 const app = express();
 const port = process.env.PORT;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, '../public/dist');
+app.use('/', express.static(publicPath));
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use(cors({
-    origin: 'http://localhost:8080',
-    credentials: true, 
-}));
-
-
 app.use('/hr', hrRoutes);
 app.use('/employee', employeeRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}...`);
