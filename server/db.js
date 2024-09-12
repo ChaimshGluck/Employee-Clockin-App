1bbc146c9d5aaf18d19ae8531b4ef7593fdf343f
Chaim Gluck <chaimshglick@gmail.com>
2024-09-11 21:48:45 -0400

    db change

--- server/db.js
+++ server/db.js
@@ -1,4 +1,3 @@
-import pgPromise from 'pg-promise';
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
@@ -9,18 +8,8 @@ const client = postgres({
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    ssl: false,
+    ssl: true,
 })
 const db = drizzle(client);
-
-// const pgp = pgPromise();
-// const db = pgp({
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_NAME,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     ssl: false,
-// })
 
 export default db;
